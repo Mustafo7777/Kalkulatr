d0d1c896a4573b35c19bb0319bc8927941d0a62b
Mustafo7777 <mustafoali7777@gmail.com>
2022-11-19 21:54:53 +0500

    added first mathematical arifmetic

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+int firstNumer = Convert.ToInt32(Console.ReadLine());
+//System.Console.WriteLine(++firstNumer);
+//System.Console.WriteLine(--firstNumer);
+firstNumer+=10;
+firstNumer-=10;
+firstNumer*=10;
+firstNumer/=10;
+System.Console.WriteLine(firstNumer);
