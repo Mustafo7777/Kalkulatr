ab68d60b28fe6e5d9733f14e1f822fa5d95be617
Mustafo7777 <118614117+Mustafo7777@users.noreply.github.com>
2022-11-20 14:36:41 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,23 +1,35 @@
 ﻿using System;
 System.Console.Write("Enter the first number: ");
 int firstNumer = Convert.ToInt32(Console.ReadLine());
+ System.Console.Write("choose one operation (+,-,*,/,%):");
+ string operation = Console.ReadLine();
 System.Console.Write("Enter the second number: ");
 int secondNumer = Convert.ToInt32(Console.ReadLine());
-//and logical operator 
+
 string message =
-  firstNumer>=0 && secondNumer>=0
-    ? "First and second numbers are  positive"
-    : "First and/or second numbers are negative";
-      System.Console.WriteLine(message);
-//or logical operator 
-string mesage =
-  firstNumer>=0 || secondNumer>=0
-    ? "First or second number is  positive"
-    : "First and second numbers are negative";
-      System.Console.WriteLine(mesage);
-//not logical operator
-string msg=
-  !(firstNumer>=0)
-    ? "First  number is not positive"
-    : "First  number is not negative";
-      System.Console.WriteLine(msg);
+  firstNumer>=0
+    ? "First number is positive"
+    :"First number is negative";
+
+
+if(firstNumer>secondNumer)
+{
+  System.Console.WriteLine("First number is greater than the second one");
+}
+else if (firstNumer==secondNumer)
+{
+  System.Console.WriteLine("First number is equal to the second one");
+}
+else {
+  System.Console.WriteLine("First number is less than the second one");
+}
+string result = operation switch
+{
+  "+" => $"{firstNumer} + {secondNumer} = {firstNumer+secondNumer}",
+  "-" => $"{firstNumer} - {secondNumer} = {firstNumer-secondNumer}",
+  "*" => $"{firstNumer} * {secondNumer} = {firstNumer*secondNumer}",
+  "/" => $"{firstNumer} / {secondNumer} = {firstNumer/secondNumer}",
+  "%" => $"{firstNumer} % {secondNumer} = {firstNumer%secondNumer}",
+  _ => "Operation not found!!!"
+};
+System.Console.WriteLine(result);
