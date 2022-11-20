6cdb9963d5e878697c88ba9d896bc10eea573376
Mustafo7777 <mustafoali7777@gmail.com>
2022-11-20 11:49:17 +0500

    Added logical operators

--- Program.cs
+++ Program.cs
@@ -3,22 +3,21 @@ System.Console.Write("Enter the first number: ");
 int firstNumer = Convert.ToInt32(Console.ReadLine());
 System.Console.Write("Enter the second number: ");
 int secondNumer = Convert.ToInt32(Console.ReadLine());
-
+//and logical operator 
 string message =
-  firstNumer>=0
-    ? "First number is positive"
-    :"First number is negative";
-
-
-if(firstNumer>secondNumer)
-{
-  System.Console.WriteLine("First number is greater than the second one");
-}
-else if (firstNumer==secondNumer)
-{
-  System.Console.WriteLine("First number is equal to the second one");
-}
-else {
-  System.Console.WriteLine("First number is less than the second one");
-}
-
+  firstNumer>=0 && secondNumer>=0
+    ? "First and second numbers are  positive"
+    : "First and/or second numbers are negative";
+      System.Console.WriteLine(message);
+//or logical operator 
+string mesage =
+  firstNumer>=0 || secondNumer>=0
+    ? "First or second number is  positive"
+    : "First and second numbers are negative";
+      System.Console.WriteLine(mesage);
+//not logical operator
+string msg=
+  !(firstNumer>=0)
+    ? "First  number is not positive"
+    : "First  number is not negative";
+      System.Console.WriteLine(msg);
